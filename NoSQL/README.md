72823940db3f871acb96e78afafd1aeee3929c22
kiat <kiat@cs.utexas.edu>
2022-04-11 22:54:33 -0500

    NoSQL

---
+++ NoSQL/README.md
@@ -0,0 +1,64 @@
+# NoSQL Databases
+
+
+# Project Voldemort
+
+A very simple NoSQL database with 3 main operations
+
+put, get, delete
+
+https://www.project-voldemort.com/voldemort/quickstart.html
+
+https://github.com/voldemort/voldemort
+
+https://github.com/voldemort/voldemort/blob/master/example/shell/example-commands
+
+https://github.com/voldemort/voldemort/blob/master/example/java/voldemort/examples/ClientExample.java
+
+
+
+# MongoDB
+
+Document-Based Database
+
+JSON objects
+
+## Install Community Edition
+
+https://www.mongodb.com/docs/manual/administration/install-community/
+
+## Mongo Shell
+
+https://www.mongodb.com/docs/mongodb-shell/
+
+
+## How to Use Python with MongoDB
+
+https://www.mongodb.com/languages/python
+
+
+# Apache Cassandra 
+
+
+https://cassandra.apache.org/_/index.html
+
+
+## Get Started with Cassandra  
+
+https://cassandra.apache.org/doc/latest/cassandra/getting_started/index.html
+
+
+## How to run Cassandra in  Docker 
+
+https://cassandra.apache.org/_/quickstart.html
+ 
+
+
+## Python Cassandra Driver
+
+https://docs.datastax.com/en/developer/python-driver/3.18/
+
+
+ 
+
+
---
+++ NoSQL/mongoDB_connect.py
@@ -0,0 +1,113 @@
+# https://www.mongodb.com/languages/python
+
+# mongosh mongo shell
+# https://www.mongodb.com/docs/mongodb-shell/run-commands/
+
+# show databases
+# use database: use students
+# search and show the content of a datbase: db.getCollection("students").find()
+# delete databse: db.dropDatabase()
+
+from pymongo import MongoClient
+import pymongo
+
+
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    # CONNECTION_STRING = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/myFirstDatabase"
+    CONNECTION_STRING = "mongodb://127.0.0.1:27017/?directConnection=true"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['students']
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+
+    # Get the database
+    dbname = get_database()
+
+    # print(str(dbname))
+
+    collection_name = dbname["students"]
+
+    student_1 = {
+      "student_id": "U1IT00001",
+      "firstname": "John",
+      "lastname": "Doe",
+      "class_year": 2017,
+      "courses": [
+        {
+          "course_id": "CS378",
+          "title": "Cloud Computing",
+          "unit": 4,
+          "tuition": 1500,
+          "sections": [
+            5051,
+            5051
+          ]
+        },
+        {
+          "course_id": "CS108",
+          "title": "Software Systems",
+          "unit": 4,
+          "tuition": 1100,
+          "sections": [
+            4011,
+            5021
+          ]
+        }
+      ]
+    }
+
+
+    student_2 = {
+      "student_id": "U1IT00002",
+      "firstname": "Mike",
+      "lastname": "Smith",
+      "class_year": 2018,
+      "courses": [
+        {
+          "course_id": "CS378",
+          "title": "Cloud Computing",
+          "unit": 4,
+          "tuition": 1500,
+          "sections": [
+            5051,
+            5051
+          ]
+        },
+        {
+          "course_id": "CS328",
+          "title": "Data Structutes",
+          "unit": 4,
+          "tuition": 1150,
+          "sections": [
+            6051,
+            7052
+          ]
+        }
+      ]
+    }
+
+
+
+    collection_name.insert_many([student_1, student_2])
+
+###########################################################
+# Reading the data back.
+
+    dbname = get_database()
+
+
+    # Create a new collection
+    collection_name = dbname["students"]
+
+    students = collection_name.find()
+
+    for student in students:
+        # This does not give a very readable output
+        print(student)
