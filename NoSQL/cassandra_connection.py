2b579a52f14122899bec6d715ced4750f8d2ec12
Kia Teymourian <kiat@cs.utexas.edu>
2024-11-04 13:10:19 -0600

    .

--- NoSQL/cassandra_connection.py
+++ NoSQL/cassandra_connection.py
@@ -7,6 +7,9 @@
 # =======
 # do the following command. 
 # docker run -d -p 9042:9042 cassandra
+# OR 
+# docker run -d -p 127.0.0.1:9042:9042 -p 127.0.0.1:9160:9160 cassandra
+
 
 # If you want to login to the container.
 # docker exec -it cassandra /bin/bash
